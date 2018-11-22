9cb52c591678045efea46f239b764f273c1763ee
Wang Yaoshen <wangyaoshen@bgi.com>
2018-11-22 16:58:40 +0800

    use excelize for read xlsx

--- xlsx2new/xlsx2new.go
+++ xlsx2new/xlsx2new.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
@@ -16,6 +17,22 @@ func main() {
 	// 根据指定路径保存文件
 	err := xlsx.SaveAs("./Book1.xlsx")
 	checkError(err)
+
+	inputXlsx, err := excelize.OpenFile("./Book1.xlsx")
+	checkError(err)
+	// 获取工作表中指定单元格的值
+	cell := inputXlsx.GetCellValue("Sheet1", "B2")
+	fmt.Println(cell)
+	// 获取Sheet1上所有单元格
+	rows := inputXlsx.GetRows("Sheet1")
+	for i, row := range rows {
+
+		fmt.Print("row:", i, "\t[")
+		for _, cell := range row {
+			fmt.Print(cell, "]\t[")
+		}
+		fmt.Println("]")
+	}
 }
 
 func checkError(e error) {
