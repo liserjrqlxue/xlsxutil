68e1243a5cd0ca31d6f76ee0662b2c1a6127a912
Wang Yaoshen <wangyaoshen@bgi.com>
2018-12-09 13:20:18 +0800

    split func to anno.go

---
+++ xlsx2new/anno.go
@@ -0,0 +1,322 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/liserjrqlxue/simple-util"
+	"github.com/tealeg/xlsx"
+	"strconv"
+	"strings"
+)
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(r.Body)
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+// anno cnv
+func annoExonCnv(sheet xlsx.Sheet, outputXlsx *xlsx.File, sheetName string) error {
+	outputSheet, err := outputXlsx.AddSheet(sheetName)
+	simple_util.CheckErr(err)
+	var keysList []string
+	var keysHash = make(map[string]bool)
+	for i, row := range sheet.Rows {
+		var outputRow = outputSheet.AddRow()
+		if i == 0 {
+			for _, cell := range row.Cells {
+				text, _ := cell.FormattedValue()
+				key := strings.Split(text, "(")[0]
+				keysList = append(keysList, key)
+				keysHash[key] = true
+			}
+			for _, title := range exonCnvAdd {
+				if !keysHash[title] {
+					keysList = append(keysList, title)
+				}
+			}
+			for _, title := range keysList {
+				outputCell := outputRow.AddCell()
+				outputCell.SetString(title)
+			}
+		} else {
+			var dataHash = make(map[string]string)
+			for j, cell := range row.Cells {
+				text, _ := cell.FormattedValue()
+				dataHash[keysList[j]] = text
+			}
+			geneArray := strings.Split(dataHash["OMIM_Gene"], ";")
+			dataHash["OMIM_Gene"] = strings.Join(geneArray, "\n")
+			var diseaseInfo []string
+			var mergeInfo = make(map[string][]string)
+			for _, gene := range geneArray {
+				if gene == "" {
+					continue
+				}
+				err = getJson(host+"/OMIM_CN?query="+gene, &diseaseInfo)
+				simple_util.CheckErr(err)
+				//fmt.Println(len(diseaseInfo))
+				if len(diseaseInfo) == 11 {
+					for i, k := range exonCnvAdd {
+						var sep = "\n"
+						if k == "GeneralizationEN" || k == "GeneralizationCH" {
+							sep = "\n\n"
+						}
+						mergeInfo[k] = append(mergeInfo[k], strings.Join(strings.Split(diseaseInfo[i], "\n"), sep))
+					}
+				}
+			}
+			for _, k := range exonCnvAdd {
+				var sep = "\n"
+				/*if k == "GeneralizationEN" || k == "GeneralizationCH" {
+					sep="\n\n"
+				}else{
+					sep="\n"
+				}*/
+				dataHash[k] = strings.Join(mergeInfo[k], sep)
+			}
+			for _, title := range keysList {
+				outputCell := outputRow.AddCell()
+				outputCell.SetString(dataHash[title])
+			}
+		}
+	}
+	return nil
+}
+
+// anno snv
+func annoSheet3(sheet xlsx.Sheet, outputXlsx *xlsx.File, sheetName string, titleList []string) error {
+	outputSheet, err := outputXlsx.AddSheet(sheetName)
+	simple_util.CheckErr(err)
+	var keysList []string
+	for i, row := range sheet.Rows {
+		var outputRow = outputSheet.AddRow()
+		if i == 0 {
+			for _, cell := range row.Cells {
+				text, _ := cell.FormattedValue()
+				keysList = append(keysList, strings.Split(text, "(")[0])
+			}
+			for _, title := range titleList {
+				//axis := positionToAxis(i, j)
+				outputCell := outputRow.AddCell()
+				outputCell.SetString(title)
+			}
+		} else {
+			var dataHash = make(map[string]string)
+			for j, cell := range row.Cells {
+				text, _ := cell.FormattedValue()
+				dataHash[keysList[j]] = text
+			}
+
+			geneSymbol := dataHash["Gene Symbol"]
+
+			// pHGVS= pHGVS1+"|"+pHGVS3
+			dataHash["pHGVS"] = dataHash["pHGVS1"] + " | " + dataHash["pHGVS3"]
+
+			score, err := strconv.ParseFloat(dataHash["dbscSNV_ADA_SCORE"], 32)
+			if err != nil {
+				dataHash["dbscSNV_ADA_pred"] = dataHash["dbscSNV_ADA_SCORE"]
+			} else {
+				if score >= 0.6 {
+					dataHash["dbscSNV_ADA_pred"] = "D"
+				} else {
+					dataHash["dbscSNV_ADA_pred"] = "P"
+				}
+			}
+			score, err = strconv.ParseFloat(dataHash["dbscSNV_RF_SCORE"], 32)
+			if err != nil {
+				dataHash["dbscSNV_RF_pred"] = dataHash["dbscSNV_RF_SCORE"]
+			} else {
+				if score >= 0.6 {
+					dataHash["dbscSNV_RF_pred"] = "D"
+				} else {
+					dataHash["dbscSNV_RF_pred"] = "P"
+				}
+			}
+
+			score, err = strconv.ParseFloat(dataHash["GERP++_RS"], 32)
+			if err != nil {
+				dataHash["GERP++_RS_pred"] = dataHash["GERP++_RS"]
+			} else {
+				if score >= 2 {
+					dataHash["GERP++_RS_pred"] = "D"
+				} else {
+					dataHash["GERP++_RS_pred"] = "P"
+				}
+			}
+
+			// 0-0.6 不保守  0.6-2.5 保守 ＞2.5 高度保守
+			score, err = strconv.ParseFloat(dataHash["PhyloP Vertebrates"], 32)
+			if err != nil {
+				dataHash["PhyloP Vertebrates Pred"] = dataHash["PhyloP Vertebrates"]
+			} else {
+				if score >= 2.5 {
+					dataHash["PhyloP Vertebrates Pred"] = "高度保守"
+				} else if score > 0.6 {
+					dataHash["PhyloP Vertebrates Pred"] = "保守"
+				} else {
+					dataHash["PhyloP Vertebrates Pred"] = "不保守"
+				}
+			}
+			score, err = strconv.ParseFloat(dataHash["PhyloP Placental Mammals"], 32)
+			if err != nil {
+				dataHash["PhyloP Placental Mammals Pred"] = dataHash["PhyloP Placental Mammals"]
+			} else {
+				if score >= 2.5 {
+					dataHash["PhyloP Placental Mammals Pred"] = "高度保守"
+				} else if score > 0.6 {
+					dataHash["PhyloP Placental Mammals Pred"] = "保守"
+				} else {
+					dataHash["PhyloP Placental Mammals Pred"] = "不保守"
+				}
+			}
+
+			dataHash["烈性突变"] = "否"
+			if LoF[dataHash["Function"]] == 1 {
+				dataHash["烈性突变"] = "是"
+			}
+
+			dataHash["HGMDorClinvar"] = "否"
+			if isHgmd.MatchString(dataHash["HGMD Pred"]) {
+				dataHash["HGMDorClinvar"] = "是"
+			}
+			if isClinvar.MatchString(dataHash["ClinVar Significance"]) {
+				dataHash["HGMDorClinvar"] = "是"
+			}
+
+			dataHash["GnomAD homo"] = dataHash["GnomAD HomoAlt Count"]
+			dataHash["GnomAD hemi"] = dataHash["GnomAD HemiAlt Count"]
+			dataHash["纯合，半合"] = dataHash["GnomAD HomoAlt Count"] // + "|" + dataHash["GnomAD HemiAlt Count"]
+			dataHash["MutationNameLite"] = dataHash["Transcript"] + ":" + strings.Split(dataHash["MutationName"], ":")[1]
+
+			dataHash["突变频谱"] = geneDb[geneSymbol]
+
+			dataHash["历史样本检出个数"] = dataHash["sampleMut"] + "/" + dataHash["sampleAll"]
+
+			// remove index
+			for _, k := range [2]string{"GeneralizationEN", "GeneralizationCH"} {
+				sep := "\n\n"
+				keys := strings.Split(dataHash[k], sep)
+				for i, _ := range keys {
+					keys[i] = indexReg.ReplaceAllLiteralString(keys[i], "")
+				}
+				dataHash[k] = strings.Join(keys, sep)
+			}
+			// add acmg
+			if acmgDb[geneSymbol] != nil {
+				acmgDbGene := acmgDb[geneSymbol]
+				if dataHash["Gene"] == "." {
+					for k, v := range geneDbHash {
+						dataHash[k] = acmgDbGene[v]
+					}
+					dataHash["SystemSort"] = "ACMG"
+				} else {
+					for k, v := range geneDbHash {
+						if k == "GeneralizationEN" || k == "GeneralizationCH" {
+							sep := "\n\n"
+							dataHash[k] = dataHash[k] + sep + acmgDbGene[v]
+						} else {
+							sep := "\n"
+							dataHash[k] = dataHash[k] + sep + acmgDbGene[v]
+						}
+					}
+					sep := "\n"
+					dataHash["SystemSort"] = dataHash["SystemSort"] + sep + "ACMG"
+				}
+			}
+
+			// 自动化判断
+			dataHash["自动化判断"] = long2short[dataHash["ACMG"]]
+
+			for _, title := range titleList {
+				outputCell := outputRow.AddCell()
+				outputCell.SetString(dataHash[title])
+			}
+		}
+	}
+	return nil
+}
+
+// copy sheet without change
+
+func copySheet(inputXlsx, outputXlsx *excelize.File, sheetName string) error {
+	inputRows := inputXlsx.GetRows(sheetName)
+	outputXlsx.NewSheet(sheetName)
+	for i, row := range inputRows {
+		for j, cell := range row {
+			axis := positionToAxis(i, j)
+			outputXlsx.SetCellValue(sheetName, axis, cell)
+		}
+	}
+	return nil
+}
+
+func copySheet2(sheet *xlsx.Sheet, outputXlsx *excelize.File, sheetName string) error {
+	outputXlsx.NewSheet(sheetName)
+	for i, row := range sheet.Rows {
+		for j, cell := range row.Cells {
+			text, _ := cell.FormattedValue()
+			axis := positionToAxis(i, j)
+			outputXlsx.SetCellValue(sheetName, axis, text)
+		}
+	}
+	return nil
+}
+
+func copySheet3(sheet xlsx.Sheet, outputXlsx *xlsx.File, sheetName string) error {
+	_, err := outputXlsx.AppendSheet(sheet, sheetName)
+	simple_util.CheckErr(err)
+	return err
+}
+
+func copySheet4(sheet xlsx.Sheet, outputXlsx *xlsx.File, sheetName string) error {
+	outputSheet, err := outputXlsx.AddSheet(sheetName)
+	simple_util.CheckErr(err)
+	for _, row := range sheet.Rows {
+		var outputRow = outputSheet.AddRow()
+		for _, cell := range row.Cells {
+			text, _ := cell.FormattedValue()
+			outputCell := outputRow.AddCell()
+			outputCell.SetString(text)
+		}
+	}
+	return err
+}
+
+func sheet2mapArray(excel *excelize.File, sheetName string) []map[string]string {
+	rows := excel.GetRows(sheetName)
+	var mapArray []map[string]string
+	var title []string
+	for i, row := range rows {
+		if i == 0 {
+			title = row
+		} else {
+			var dataHash = make(map[string]string)
+			for j, cell := range row {
+				dataHash[title[j]] = cell
+			}
+			mapArray = append(mapArray, dataHash)
+		}
+	}
+	return mapArray
+}
+
+func sheet2mapHash(excel *excelize.File, sheetName, key string) map[string]map[string]string {
+	rows := excel.GetRows(sheetName)
+	var mapHash = make(map[string]map[string]string)
+	var title []string
+	for i, row := range rows {
+		if i == 0 {
+			title = row
+		} else {
+			var dataHash = make(map[string]string)
+			for j, cell := range row {
+				dataHash[title[j]] = cell
+			}
+			mapHash[dataHash[key]] = dataHash
+		}
+	}
+	return mapHash
+}
